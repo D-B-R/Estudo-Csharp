0af816302eaa6382757358cb166bdbea12c7fbee
D-B-R <daniel.b.rocha@hotmail.com>
2021-12-20 09:38:58 -0300

    2.0

--- 2--PP/5-PP/5-PP.cs
+++ 2--PP/5-PP/5-PP.cs
@@ -11,7 +11,7 @@ namespace _5_PP
         static void Main(string[] args)
         {
 
-            Console.WriteLine(" Teste ");
+            Console.WriteLine(" A ");
 
             char primeiraLetra = 'a';
 
